5cacaff73b51c7769d1180f0e50dbbf05f29f56d
Sauvik <rsauvik9@gmail.com>
2025-10-26 21:52:19 +0530

    feat:added about & contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,22 @@
 import { useState } from "react";
 import Products from "./components/products/Products";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import NavBar from "./components/shared/Navbar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
 export default function App() {
   const [count, setCount] = useState(0);
-  return <Products />;
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
--- frontend/src/components/shared/ProductCard.jsx
+++ frontend/src/components/shared/ProductCard.jsx
@@ -12,6 +12,7 @@ const ProductCard = ({
   price,
   discount,
   specialPrice,
+  about = false,
 }) => {
   const [openProductViewModal, setOpenProductViewModal] = useState(false);
   const btnLoader = false;
@@ -21,8 +22,10 @@ const ProductCard = ({
   // console.log("Product Image URL:", image);
 
   const handleProductView = (product) => {
-    setSelectedViewProduct(product);
-    setOpenProductViewModal(true);
+    if (!about) {
+      setSelectedViewProduct(product);
+      setOpenProductViewModal(true);
+    }
   };
   return (
     <div className="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300">
@@ -71,34 +74,46 @@ const ProductCard = ({
           </p>
         </div>
 
-        <div className="flex items-center justify-between">
-          {specialPrice ? (
-            <div className="flex flex-col">
-              <span className="text-gray-400 line-through">
-                ${Number(price).toFixed(2)}
-              </span>
+        {!about && (
+          <div className="flex items-center justify-between">
+            {specialPrice ? (
+              <div className="flex flex-col">
+                <span className="text-gray-400 line-through">
+                  ${Number(price).toFixed(2)}
+                </span>
+                <span className="text-xl font-bold text-slate-700">
+                  ${Number(specialPrice).toFixed(2)}
+                </span>
+              </div>
+            ) : (
               <span className="text-xl font-bold text-slate-700">
-                ${Number(specialPrice).toFixed(2)}
+                {"  "}${Number(price).toFixed(2)}
               </span>
-            </div>
-          ) : (
-            <span className="text-gray-400 line-through">
-              {" "}
-              ${Number(price).toFixed(2)}
-            </span>
-          )}
-          <button
-            disabled={!isAvailable || btnLoader}
-            onClick={() => {}}
-            className={`bg-blue-500 ${
-              isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"
-            }
-            text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}
-          >
-            <FaShoppingCart className="mr-2" />
-            {isAvailable ? "Add to Cart" : "Stock Out"}
-          </button>
-        </div>
+            )}
+
+            <button
+              disabled={!isAvailable || btnLoader}
+              onClick={() =>
+                addToCartHandler({
+                  image,
+                  productName,
+                  description,
+                  specialPrice,
+                  price,
+                  productId,
+                  quantity,
+                })
+              }
+              className={`bg-blue-500 ${
+                isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"
+              }
+                        text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}
+            >
+              <FaShoppingCart className="mr-2" />
+              {isAvailable ? "Add to Cart" : "Stock Out"}
+            </button>
+          </div>
+        )}
       </div>
       <ProductViewModal
         open={openProductViewModal}
